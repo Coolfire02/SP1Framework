4beecf568ecfd89901c26fff63c5b6c5e30c9590
coolfire02 - laptop <coolfire02@vortexnetwork.me>
2020-08-19 11:47:36 +0800

    Level Declar by YH

--- SP1Framework/Level.h
+++ SP1Framework/Level.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "GameObject.h"
+
 class Level
 {
+private:
+	GameObject* objs; 
 public:
 	Level();
 	~Level();
+};
+
+enum LEVELS {
+	MAIN,
+	TUTORIAL,
+	STAGE_1_LEVEL_1,
+	STAGE_2_LEVEL_1,
+	STAGE_BUILDER,
+	GRAPHICS_BUILDER
 };
 
